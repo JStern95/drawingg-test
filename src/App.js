ebd549650d552b1b45e8b85551853a24fb4efc91
JStern95 <JSternberg95@gmail.com>
2018-11-08 18:13:33 -0500

    With sketchpad

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {SketchField, Tools} from 'react-sketch';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class SketchFieldDemo extends React.Component {
+
+  // state={
+  //   width='1024px',
+  //   height='200px',
+  //   tool={Tools.Pencil},
+  //   lineColor='pink',
+  //   lineWidth={3}
+  // }
+
+     render() {
+        return (
+          <div onMouseUp={()=>console.log("You stopped drawing")}>
+            <SketchField width='50%'
+                                 height='50%'
+                                 tool={Tools.Pencil}
+                                 lineColor='red'
+                                 lineWidth={3}/>
+            </div>
+        )
+     }
 }
 
-export default App;
+export default SketchFieldDemo;
